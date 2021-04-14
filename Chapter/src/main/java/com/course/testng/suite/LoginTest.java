80089ab44cb5378f534024e1c479503f78fb9fed
lzy008 <279644115@qq.com>
2021-04-14 23:58:15 +0800

    20210414第二次提交，testng参数化

---
+++ Chapter/src/main/java/com/course/testng/suite/LoginTest.java
@@ -0,0 +1,11 @@
+package com.course.testng.suite;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    //测试Test之前的方法
+    @Test
+    public void loginTaoBao(){
+        System.out.println("淘宝登录成功");
+    }
+}
